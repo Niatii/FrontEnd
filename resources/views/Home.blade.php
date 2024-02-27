d42c311fe0e5bcd1d75f23e6526a8c90a32b0755
Niati <niatiniati545@gmail.com>
2024-02-27 20:14:16 +0700

    first commit

---
+++ resources/views/Home.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @vite('resources/css/app.css')
+    <title>Home</title>
+</head>
+<body>
+    <!-- Header Start -->
+    <header class="bg-blue absolute top-0 left-0 w-full flex items-center z-10">
+        <div class="container">
+            <div class="felx items-center justify-between relative">
+                <div class="px-4">
+                    <a href="#home" class="font-blod text-lg block py-6">Logo</a>
+                    <a href="#home" class="font-blod text-lg block py-6">Logo</a>
+                </div>
+            </div>
+        </div>
+    </header>
+    <!-- Header End -->
+</body>
+</html>
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./resources/**/*.blade.php",
+    "./resources/**/*.js",
+    "./resources/**/*.vue",
+  ],
+  theme: {
+    colors: {
+      'blue': '#5268B9',
+      'white': '#D9D9D9',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
