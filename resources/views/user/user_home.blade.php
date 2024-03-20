eba48f8e1b6da202aaae80fac1aa5a912daff320
lilChiie <shifa.hst@email.com>
2024-03-20 22:20:58 +0700

    perbaikan route

--- resources/views/user/user_home.blade.php
+++ resources/views/user/user_home.blade.php
@@ -3,6 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    @vite(['resources/css/app.css','resources/js/app.js'])
+
     @vite('resources/css/app.css')
 
     <!-- font Family -->
@@ -17,12 +20,13 @@
     <!-- library fontawesome -->
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" rel="stylesheet">
 
-
-    <title>Home</title>
+    <!-- AOS -->
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+    
 </head>
-<body class="bg-latar">
+<body class="bg-latar text-black">
     <!-- Header Start -->
-    <header class="w-full bg-transparent fixed top-0 left-0 h-28">
+    <header class="w-full bg-transparent fixed top-0 left-0 h-28 z-50">
         <div class="container">
             <div class="bg-nav w-full absolute top-0 left-0 justify-between flex">
                 <div class="flex">
@@ -31,13 +35,16 @@
                     <a href="#"><img src="{{ asset('storage/properti/1.jpg') }}" alt="gambar properti"
                     class="w-8 my-2 mx-1 rounded-full sm:w-10 sm:ml-3"></a>
                 </div>
-                <!-- <div class="grow">
-                <input type="search" class="mt-3 ml-4 w-44 p-1 text-xs focus:outline-none focus:ring focus:ring-indigo-100
-                rounded-sm sm:w-8/12 sm:ml-10 sm:mt-2 md:max-w-[592px] sm:text-base" />
-                </div> -->
-                <div>
-                <a href="/login"><img src="{{ asset('storage/properti/1.jpg') }}" alt="gambar properti"
-                    class="w-8 my-2 mx-2 rounded-lg  sm:w-10 sm:ml-10 sm:mr-4 md:ml-24"></a>
+                <div class="relative text-black">
+                    <img src="{{ asset('storage/properti/3.jpg') }}" alt="gambar properti" class="w-8 my-2 mx-2 rounded-full sm:w-10 sm:ml-10 sm:mr-4 md:ml-24
+                    object-cover h-10"
+                    id="user" >
+                    <ul class="dropdown bg-fot absolute top-0 right-0 mt-10 lg:mt-12 py-3 px-4 mr-5 lg:px-8 shadow-md z-20 hidden
+                    transition duration-700"
+                    id="dropdownuser">
+                        <li class="my-1 lg:my-2"><a href="#profil" class="hover:text-sky-600">Profil</a></li>
+                        <li class="my-1 lg:my-2"><a href="#logout" class="hover:text-sky-600">Logout</a></li>
+                    </ul>
                 </div>
             </div>
             <!-- navbar start -->
@@ -53,61 +60,36 @@
                         <ul class="block lg:flex">
                             <li class="group">
                                 <a href="/" class="text-base text-black py-2 mx-8 flex group-hover:text-sky-600
-                                lg:mx-5" id="navhome">Home</a>
+                                lg:mx-5 {{ request()->is('/') ? 'text-sky-600' : 'text-black' }}" id="navhome">Home</a>
                             </li>
                             <li class="group relative">
-                                <a href="/tentangkami" class="text-base text-black py-2 mx-8 flex group-hover:text-sky-600
+                                <a href="#" class="text-base text-black py-2 mx-8 flex group-hover:text-sky-600
                                 lg:mx-5" id="tentangkami">Tentang Kami</a>
-                                <ul class="dropdown bg-latar lg:bg-fot hidden lg:absolute py-3 px-8 lg:ml-8 lg:px-5 lg:pt-5 lg:shadow-md lg:rounded-md" 
-                                id="dropdowntentangkami">
-                                    <li><a href="#contoh1" class="hover:text-sky-600 ">Example</a></li>
-                                    <li><a href="#contoh2" class="hover:text-sky-600 ">Example</a></li>
-                                    <li><a href="#contoh3" class="hover:text-sky-600 ">Example</a></li>
-                                    <li><a href="#contoh4" class="hover:text-sky-600 ">Example</a></li>
-                                </ul>
+                                
                             </li>
                             <li class="group relative ">
-                                <a href="/konsultasi" class="text-base text-black py-2 mx-8 flex group-hover:text-sky-600
+                                <a href="#" class="text-base text-black py-2 mx-8 flex group-hover:text-sky-600
                                 lg:mx-5" id="konsultasi">Konsultasi</a>
-                                <ul class="dropdown bg-latar lg:bg-fot hidden lg:absolute py-3 px-8 lg:ml-5 lg:px-5 lg:pt-5 lg:shadow-md lg:rounded-md" 
-                                id="dropdownkonsultasi">
-                                    <li><a href="#contoh5" class="hover:text-sky-600 ">Example</a></li>
-                                    <li><a href="#contoh6" class="hover:text-sky-600 ">Example</a></li>
-                                    <li><a href="#contoh7" class="hover:text-sky-600 ">Example</a></li>
-                                    <li><a href="#contoh8" class="hover:text-sky-600 ">Example</a></li>
-                                </ul>
+                                
                             </li>
                             <li class="group relative">
-                                <a href="#riset" class="text-base text-black py-2 mx-8 flex group-hover:text-sky-600
+                                <a href="#" class="text-base text-black py-2 mx-8 flex group-hover:text-sky-600
                                 lg:mx-5" id="riset">Riset</a>
-                                <ul class="dropdown bg-latar lg:bg-fot hidden lg:absolute py-3 px-8 lg:ml-5 lg:px-5 lg:pt-5 lg:shadow-md lg:rounded-md"
-                                id="dropdownriset">
-                                    <li><a href="#contoh9" class="hover:text-sky-600 ">Example</a></li>
-                                    <li><a href="#contoh10" class="hover:text-sky-600 ">Example</a></li>
-                                    <li><a href="#contoh11" class="hover:text-sky-600 ">Example</a></li>
-                                    <li><a href="#contoh12" class="hover:text-sky-600 ">Example</a></li>
-                                </ul>
+                               
                             </li>
                             <li class="group relative">
                                 <a href="#publikasi" class="text-base text-black py-2 mx-8 flex group-hover:text-sky-600
                                 lg:mx-6" id="publikasi">Publikasi</a>
-                                <ul class="dropdown bg-latar lg:bg-fot hidden lg:absolute py-3 px-8 lg:ml-5 lg:px-5 lg:pt-5 lg:shadow-md lg:rounded-md"
-                                id="dropdownpublikasi">
-                                    <li><a href="#contoh13" class="hover:text-sky-600 ">Example</a></li>
-                                    <li><a href="#contoh14" class="hover:text-sky-600 ">Example</a></li>
-                                    <li><a href="#contoh15" class="hover:text-sky-600 ">Example</a></li>
-                                    <li><a href="#contoh16" class="hover:text-sky-600 ">Example</a></li>
-                                </ul>
+                                
                             </li>
-                            <li class="group relative">
-                                <a href="#akademi" class="text-base text-black py-2 mx-8 flex group-hover:text-sky-600
+                            <li class="group relative text-black ">
+                                <a href="#akademi" class="text-base py-2 mx-8 flex group-hover:text-sky-600
                                 lg:mx-6" id="akademi">Akademi</a>
-                                <ul class="dropdown bg-latar lg:bg-fot hidden lg:absolute py-3 px-8 lg:ml-5 lg:px-5 lg:pt-5 lg:shadow-md lg:rounded-md"
+                                <ul class="dropdown bg-latar lg:bg-fot hidden lg:absolute py-3 px-8 lg:ml-5 lg:px-5 lg:pt-5 lg:shadow-md lg:rounded-md
+                                w-full transition duration-700"
                                 id="dropdownakademi">
-                                    <li><a href="#contoh17" class="hover:text-sky-600 ">Example</a></li>
-                                    <li><a href="#contoh18" class="hover:text-sky-600 ">Example</a></li>
-                                    <li><a href="#contoh19" class="hover:text-sky-600 ">Example</a></li>
-                                    <li><a href="#contoh20" class="hover:text-sky-600 ">Example</a></li>
+                                    <li class="my-2"><a href="#pelatihan" class="hover:text-sky-600">Pelatihan</a></li>
+                                    <li class="my-2"><a href="#kegiatan" class="hover:text-sky-600 ">Kegiatan</a></li>
                                 </ul>
                             </li>
                             <li class="group relative">
@@ -125,52 +107,34 @@
         </div>
     </header>
     <!-- Header End -->
-
-    <!-- Hero Section Start -->
-    <section class="pt-36 sm:pt-40 mb-12 mx-8 flex justify-center">
-        <div class="container bg-white pl-4 pb-8 lg:pl-7 rounded-md ">
-            <div class="pl-2">
-                <p class=" pt-4 sm:text-lg lg:text-2xl sm:pt-6">Title Page</p>
-                <hr class="border-t-1 border-black w-24 pb">
+    <!-- Content Start -->
+    <section class="pt-32 sm:pt-40 mb-18">
+        <form class="w-8/12 mx-3">   
+            <label for="default-search" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
+            <div class="relative">
+                <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+                    <svg class="w-4 h- text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
+                    </svg>
+                </div>
+                <input type="search" id="default-search" class="block w-full p-2 ps-10 text-xs lg:text-sm text-gray-900 border
+                 border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search..." required />
+                <button type="submit" class="text-white absolute end-0.5 bottom-1 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-base rounded-lg text-xs lg:text-sm px-3 py-1 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button>
             </div>
-            <div class="my-4 mx-2 w-11/12 grid grid-cols-2 gap-x-11 gap-y-2">
-                <div class="">
-                    <img src="{{ asset('storage/properti/2.jpg') }}" alt=""
-                    class="w-32 sm:w-72">
-                    <p class="text-xs md:text-lg sm:mb-3">Title foto</p>
-                    <p class="text-[10px] h-8 md:h-12 mb-1 md:mb-2 overflow-hidden md:text-xs
-                    ">Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore, fugit amet hic dolore ab voluptatum, sit iste fuga repellat a, at soluta corrupti. Corporis quae sint tenetur libero earum omnis, commodi deleniti odit sed assumenda at quaerat. Voluptas ratione vel amet ipsum tempore odit minima pariatur, laudantium repudiandae fugiat cumque!</p>
-                </div>
-                <div class="">
-                    <img src="{{ asset('storage/properti/2.jpg') }}" alt=""
-                    class="w-32 sm:w-72">
-                    <p class="text-xs md:text-lg sm:mb-3">Title foto</p>
-                    <p class="text-[10px] h-8 md:h-12 mb-1 md:mb-2 overflow-hidden md:text-xs
-                    ">Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore, fugit amet hic dolore ab voluptatum, sit iste fuga repellat a, at soluta corrupti. Corporis quae sint tenetur libero earum omnis, commodi deleniti odit sed assumenda at quaerat. Voluptas ratione vel amet ipsum tempore odit minima pariatur, laudantium repudiandae fugiat cumque!</p>
-                </div>
-                <div class="">
-                    <img src="{{ asset('storage/properti/2.jpg') }}" alt=""
-                    class="w-32 sm:w-72">
-                    <p class="text-xs md:text-lg sm:mb-3">Title foto</p>
-                    <p class="text-[10px] h-8 md:h-12 mb-1 md:mb-2 overflow-hidden md:text-xs
-                    ">Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore, fugit amet hic dolore ab voluptatum, sit iste fuga repellat a, at soluta corrupti. Corporis quae sint tenetur libero earum omnis, commodi deleniti odit sed assumenda at quaerat. Voluptas ratione vel amet ipsum tempore odit minima pariatur, laudantium repudiandae fugiat cumque!</p>
-                </div>
-                <div class="">
-                    <img src="{{ asset('storage/properti/2.jpg') }}" alt=""
-                    class="w-32 sm:w-72">
-                    <p class="text-xs md:text-lg sm:mb-3">Title foto</p>
-                    <p class="text-[10px] h-8 md:h-12 mb-1 md:mb-2 overflow-hidden md:text-xs
-                    ">Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore, fugit amet hic dolore ab voluptatum, sit iste fuga repellat a, at soluta corrupti. Corporis quae sint tenetur libero earum omnis, commodi deleniti odit sed assumenda at quaerat. Voluptas ratione vel amet ipsum tempore odit minima pariatur, laudantium repudiandae fugiat cumque!</p>
-                </div>
-            </div>
-            
-           
+        </form>
+        <div  class="grid mx-10 my-12 gap-x-8 gap-y-8 md:grid-cols-3">
+            <img src="{{ asset('storage/properti/4.jpg') }}" alt="gambar properti" class="transition-all duration-300 rounded-lg hover:brightness-50" data-aos="fade-right">
+            <img src="{{ asset('storage/properti/4.jpg') }}" alt="gambar properti" class="transition-all duration-300 rounded-lg hover:brightness-50" data-aos="fade-up">
+            <img src="{{ asset('storage/properti/4.jpg') }}" alt="gambar properti" class="transition-all duration-300 rounded-lg hover:brightness-50" data-aos="fade-left">
+        </div>
+        <div class="bg-white">
+            <p class="text-center font-serif py-10 text-lg md:text-2xl lg:text-4xl">Selamat Datang di Web Tax Digital</p>
         </div>
     </section>
-    <!-- Hero Section End -->
-
+    
+    <!-- Content End -->
     <!-- Footer Start -->
-    <footer class="bg-fot">
+    <footer class="bg-fot text-black mt-20" data-aos="fade-up">
         <div class="container max-w-full pt-2">
             <div class="grid grid-cols-4 lg:grid-cols-5 gap-3">
                 <div class="mx-2 text-xs sm:text-base sm:mx-12 lg:mr-12 w-8/12">
@@ -215,7 +179,7 @@
                 </div>
         
             </div>
-            <div class="mt-8 mx-2 lg:mt-12 lg:ml-12">
+            <div class="mt-8 mx-2 sm:mx-12  lg:mt-12 lg:ml-12">
                 <p class="font-bold text-base lg:text-2xl">Link Reference</p>
                 <div class="flex">
                     <a href="#"><i class="fab fa-facebook mr-2 lg:text-xl"></i></a>
@@ -224,7 +188,7 @@
                     <a href="#"><i class="fab fa-youtube lg:text-xl"></i></a>
                     <p class="text-[8px] font-sans mt-3 ml-5 lg:text-sm">No Telp 08xx-xxxx-xxx</p>
                 </div>
-                <div class="mt-14 pb-8 text-[6px] lg:text-xs">
+                <div class="mt-14 pb-8 text-[6px] lg:text-xs ">
                 <p class="text-center"><i class="far fa-copyright"></i> COPYRIGHT 2024 TAX Digital. All rights reserved. Not for further distribution without the permission of TAX digital</p>
                 </div>
                 
@@ -233,7 +197,13 @@
     </footer>
     <!-- Footer End -->
 
+    
+
     <!-- javascript -->
+    <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+    <script>
+    AOS.init();
+    </script>
     @vite('resources/js/app.js')
 </body>
 </html>
